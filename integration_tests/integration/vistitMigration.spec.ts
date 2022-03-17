20a33367d4971a3259cdf52f79a2f58e49aade02
Andy Marke <andy.marke@digital.justice.gov.uk>
2022-03-17 16:54:04 +0000

    SDI-119 Test for tile and home page

---
+++ integration_tests/integration/vistitMigration.spec.ts
@@ -0,0 +1,35 @@
+import IndexPage from '../pages/index'
+import Page from '../pages/page'
+import VisitsMigrationPage from '../pages/visits-migration/visitsMigration'
+
+context('Visit Migration Homepage', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubAuthUser')
+  })
+  context('With MIGRATE_VISITS role', () => {
+    beforeEach(() => {
+      cy.task('stubSignIn', ['ROLE_MIGRATE_VISITS'])
+      cy.signIn()
+    })
+    it('should see migrate visits tile', () => {
+      const indexPage = Page.verifyOnPage(IndexPage)
+      indexPage.visitsMigrationLink().should('be.visible')
+    })
+    it('should be able to navigate to the visits migration home page', () => {
+      const indexPage = Page.verifyOnPage(IndexPage)
+      indexPage.visitsMigrationLink().click()
+      Page.verifyOnPage(VisitsMigrationPage)
+    })
+  })
+  context('Without MIGRATE_VISITS role', () => {
+    beforeEach(() => {
+      cy.task('stubSignIn', ['ROLE_MIGRATE_PRISONERS'])
+      cy.signIn()
+    })
+    it('should not see migrate visits tile', () => {
+      const indexPage = Page.verifyOnPage(IndexPage)
+      indexPage.visitsMigrationLink().should('not.exist')
+    })
+  })
+})
---
+++ integration_tests/pages/visits-migration/visitsMigration.ts
@@ -0,0 +1,7 @@
+import Page from '../page'
+
+export default class VisitsMigrationPage extends Page {
+  constructor() {
+    super('Visits migration')
+  }
+}
