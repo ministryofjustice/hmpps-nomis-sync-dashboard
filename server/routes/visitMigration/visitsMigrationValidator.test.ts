8b7dff369dbd799b349eae46a9a0a4b166ae4bc3
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2022-03-28 14:44:25 +0100

    sdi-133 add moj filter for migration view

---
+++ server/routes/visitMigration/visitsMigrationValidator.test.ts
@@ -0,0 +1,60 @@
+import validate from './visitsMigrationValidator'
+import { MigrationViewFilter } from '../../@types/dashboard'
+
+describe('visitsMigrationValidator', () => {
+  it('should no errors when everything is valid', () => {
+    const form: MigrationViewFilter = validForm
+
+    expect(validate(form)).toHaveLength(0)
+  })
+
+  it('should allow an empty prisonId', () => {
+    const form: MigrationViewFilter = {
+      ...validForm,
+      prisonId: '',
+    }
+
+    expect(validate(form)).toHaveLength(0)
+  })
+
+  it('should reject invalid fromDateTime', () => {
+    const form: MigrationViewFilter = {
+      ...validForm,
+      fromDateTime: 'invalid',
+    }
+
+    expect(validate(form)).toContainEqual({
+      href: '#fromDateTime',
+      text: 'Enter a real date time, like 2020-03-23T12:00:00 or 2020-03-23',
+    })
+  })
+
+  it('should reject invalid toDateTime', () => {
+    const form: MigrationViewFilter = {
+      ...validForm,
+      toDateTime: 'invalid',
+    }
+
+    expect(validate(form)).toContainEqual({
+      href: '#toDateTime',
+      text: 'Enter a real date time, like 2020-03-23T12:00:00 or 2020-03-23',
+    })
+  })
+
+  it('should allow just date in to/fromDateTime', () => {
+    const form: MigrationViewFilter = {
+      ...validForm,
+      fromDateTime: '2020-03-23',
+      toDateTime: '2020-03-23',
+    }
+
+    expect(validate(form)).toHaveLength(0)
+  })
+})
+
+const validForm: MigrationViewFilter = {
+  prisonId: 'HEI',
+  fromDateTime: '2020-03-23T12:00:00',
+  toDateTime: '2020-03-24T12:00:00',
+  includeOnlyFailures: false,
+}
