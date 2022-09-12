576a3c8ce9e9f81028d7dbe0be07b1df9ad9d4b6
petergphillips <peter@greenthistle.co.uk>
2022-09-12 10:53:34 +0100

    SDI-367: ⚡ Speed up test improvements copied from typescript template project (#57)

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -1,4 +1,6 @@
-Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
+import VisitOptions = Cypress.VisitOptions
+
+Cypress.Commands.add('signIn', (options?: Partial<VisitOptions>) => {
   cy.request(`/`)
   cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
 })
