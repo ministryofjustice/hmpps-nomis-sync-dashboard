b3ada532ebe80048b68d5a821f27b9847be849ce
karenmillermoj <karen.miller@digital.justice.gov.uk>
2025-01-23 11:46:48 +0000

    SDIT-2399 Remove unused files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,7 @@ import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 export default hmppsConfig()
 /* TODO add these?
-node_modules
 public
-assets
-cypress.json
 reporter-config.json
 dist/
 
