b36c7bc7317303b23d12b07a4fdd57d455f898ae
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2025-04-24 15:30:40 +0100

    fixed test

--- integration_tests/pages/visits-room-mapping/addRoomMappings.ts
+++ integration_tests/pages/visits-room-mapping/addRoomMappings.ts
@@ -2,7 +2,7 @@ import Page, { PageElement } from '../page'
 
 export default class AddRoomMappingPage extends Page {
   constructor(prisonId: string, roomId: string) {
-    super(`Add room mapping for Nomis room: ${roomId} at Prison ${prisonId}`)
+    super(`Add room mapping for NOMIS room - ${prisonId}`)
   }
 
   vsipIdEntry = () => cy.get('#vsipId')
