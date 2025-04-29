febe77733abd321c08299d38fa3b0d8f4bfd4552
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-04-29 13:44:21 +0100

    SDIT-2709: ♻️ Switch to hmpps rest client (6)

--- server/routes/testutils/mockActivitiesService.ts
+++ server/routes/testutils/mockActivitiesService.ts
@@ -1,8 +1,7 @@
 import ActivitiesService from '../../services/activitiesService'
-import HmppsAuthClient from '../../data/hmppsAuthClient'
 
 jest.mock('../../services/activitiesService')
 
-const activitiesService = new ActivitiesService({} as HmppsAuthClient) as jest.Mocked<ActivitiesService>
+const activitiesService = new ActivitiesService(null) as jest.Mocked<ActivitiesService>
 
 export default activitiesService
