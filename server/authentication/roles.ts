3fdd2c1c7cd7f61a1a98b714f04b0451871069f7
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-10-14 10:49:50 +0100

    SDIT-3103: 🛂 Switch to explicit use of activities role

--- server/authentication/roles.ts
+++ server/authentication/roles.ts
@@ -1,8 +1,8 @@
 import type { Response } from 'express'
 import { jwtDecode } from 'jwt-decode'
 
+// This role is deliberately shared with Allocations as they are closely related
 const MIGRATE_ACTIVITIES_ROLE = 'ROLE_MIGRATE_ACTIVITIES'
-const MIGRATE_ALLOCATIONS_ROLE = 'ROLE_MIGRATE_ACTIVITIES' // This role is deliberately shared with Activities as they are closely related
 const MIGRATE_APPOINTMENTS_ROLE = 'ROLE_MIGRATE_APPOINTMENTS'
 const MIGRATE_NOMIS_SYSCON = 'ROLE_MIGRATE_NOMIS_SYSCON'
 const MIGRATE_SENTENCING_ROLE = 'ROLE_MIGRATE_SENTENCING'
@@ -18,7 +18,6 @@ export {
   MIGRATE_VISITS_ROLE,
   MIGRATE_SENTENCING_ROLE,
   MIGRATE_ACTIVITIES_ROLE,
-  MIGRATE_ALLOCATIONS_ROLE,
   MIGRATE_APPOINTMENTS_ROLE,
   MIGRATE_NOMIS_SYSCON,
   extractRoles,
