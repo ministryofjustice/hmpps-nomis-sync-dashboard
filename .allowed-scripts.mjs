bc376c47123684e44a9df4da2db1f1dd06db260f
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-12-05 08:26:16 +0000

    SDIT-3254: 🔒️ Upgrade packages for jwt security vuln

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -7,11 +7,11 @@ export default configureAllowedScripts({
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.27.0': 'ALLOW',
+    'node_modules/esbuild@0.27.1': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Need by cypress for installation etc.
-    'node_modules/cypress@15.6.0': 'ALLOW',
+    'node_modules/cypress@15.7.1': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
